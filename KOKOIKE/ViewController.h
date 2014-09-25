fc4f5164386bbe486025ea029c0d7ea0604edcf3
smartshinobu <bizan07@bizankomumakku07-no-MacBook-Air-2.local>
2014-09-25 13:21:39 +0900

    Initial Commit

---
+++ KOKOIKE/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KOKOIKE
+//
+//  Created by ビザンコムマック０７ on 2014/09/25.
+//  Copyright (c) 2014年 mycompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
